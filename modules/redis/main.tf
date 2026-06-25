d529167030348833fd41b9eba3299d05b9bb74c4
Bobbiti Yaswanth Reddy <yaswanthreddybobbiti16@gmail.com>
2026-06-25 08:22:58 +0530

    Change Managed Redis SKU to Balanced_B3 due to capacity limitations for B0/B1 in East US 2

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_managed_redis" "redis" {
   name                = var.redis_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku_name            = "Balanced_B1"
+  sku_name            = "Balanced_B3"
 
   default_database {
   }
