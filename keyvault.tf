7115618cb713dc895ee370cda6a0c857898870cf
Bobbiti Yaswanth Reddy <yaswanthreddybobbiti16@gmail.com>
2026-06-25 15:44:43 +0530

    Fix VM system identity output: use identity[0] instead of one() splat to avoid null during plan

--- keyvault.tf
+++ keyvault.tf
@@ -83,5 +83,6 @@ resource "azurerm_role_assignment" "vm_secrets_user" {
   scope                = module.keyvault.resource_id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = module.vm.vm_identity_principal_id
+  principal_type       = "ServicePrincipal"
 }
 
