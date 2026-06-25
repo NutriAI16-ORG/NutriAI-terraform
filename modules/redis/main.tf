a8287f4def2753189d6bbc60e0a4f6d507afa813
Bobbiti Yaswanth Reddy <yaswanthreddybobbiti16@gmail.com>
2026-06-25 07:14:45 +0530

    Fix deprecated enable_non_ssl_port argument to non_ssl_port_enabled

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_redis_cache" "redis" {
   capacity            = 1
   family              = "C"
   sku_name            = "Standard"
-  enable_non_ssl_port = false
+  non_ssl_port_enabled = false
   minimum_tls_version = "1.2"
 
   redis_configuration {
